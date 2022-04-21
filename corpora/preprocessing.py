5acfe1f2c75e1a6c0e7c23502155b17b92c17eb2
deepansha-singh <deepansha.singh5@gmail.com>
2022-04-21 13:32:34 +0530

    added final code for case study

--- corpora/preprocessing.py
+++ corpora/preprocessing.py
@@ -1,10 +1,52 @@
+"""
+code for preprocessing the manually processed
+corpus data comprising of raw text from the
+selected sample novels
+
+Manual processing includes removal of
+metadata, acnowledgements, indexes, etc.
+from the downloaded raw files
+
+It is then sent to this code file for
+the preprocessing, including:
+file handling, punctuation removal,
+stop word removal in accordance to the
+nltk package, which is then finally followed by
+the generation of a frequency dictionary for each
+significant word in the text
+"""
+
+"""
+NLTK is one of the most prominently used
+language toolkits applied in the study of
+texts via stylometry
+It is used to work with human language data,
+alongside many corpora and lexical resources
+
+here we import the stopwords corpora
+from the many available resources to
+work around our own corpus preprocessing
+"""
+
 from nltk.corpus import stopwords
-from nltk.probability import FreqDist
 
 from string import punctuation, digits
 
 class Preprocessing:
+  """
+  Preprocessing documented text string for initialization, 
+  preparing, and increasing the usability of a utf-8
+  encoded raw input
+
+  Parameters
+  ----------
+  filename: string
+    The sent variable string from the class call line
+    with the file name of the raw text file
+  """
+
   def __init__(self, filename):
+    # Over-writing the filename variable with the path of the specified file name
     if filename == 'rom1':
       self.filename = r'D:/git_repos/Stylometric-Visualizations/corpora/test1-romance/rom1_TheProblemWithForever.txt'
     elif filename == 'rom2':
@@ -20,6 +62,10 @@ class Preprocessing:
     return content.lower()
 
   def stop_words(self, word_list):
+    """
+    
+    """
+    
     stop_words = set(stopwords.words('english'))
     filtered = [word for word in word_list if not word in stop_words]
     return filtered
